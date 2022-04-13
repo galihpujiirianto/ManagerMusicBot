890130855f18bfcf48db1ada89192f1e90deb618
Galih <galihpujiirianto@gmail.com>
2022-04-13 09:39:24 +0700

    update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -205,7 +205,7 @@ def primary_markup(videoid, user_id, current_time, total_time):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{current_time} ------------------ {total_time}",
+                text=f"{(total_time - current_time)} ------------------ {total_time}",
                 callback_data=f"timer_checkup_markup {videoid}|{user_id}",
             )
         ],
@@ -229,7 +229,7 @@ def timer_markup(videoid, user_id, current_time, total_time):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{current_time} ------------------ {total_time}",
+                text=f"{(total_time - current_time)} ------------------ {total_time}",
                 callback_data=f"timer_checkup_markup {videoid}|{user_id}",
             )
         ],
@@ -256,7 +256,7 @@ def audio_markup(videoid, user_id, current_time, total_time):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{current_time} ------------------ {total_time}",
+                text=f"{(total_time - current_time)} ------------------ {total_time}",
                 callback_data=f"timer_checkup_markup {videoid}|{user_id}",
             )
         ],
